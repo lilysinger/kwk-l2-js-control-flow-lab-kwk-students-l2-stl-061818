25779cd2a247fb41dd0b38959331f3d5d3c7ed8c
lilysinger <lilymsinger@gmail.com>
2018-06-22 18:36:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code in this file!
+function scuberGreetingForFeet(feet){
+  
+  if (feet <= 400) {
+    return "This one is on me!";
+  } 
+  else if (1999 < feet && feet <2500) {
+    return "I will gladly take your thirty bucks.";
+  } 
+  else {
+    return "No can do.";
+  }
+}
+
+
+
+
+  
+
+function ternaryCheckCity(){
+  return 1
+  
+}
+
+function switchOnCharmFromTip(){
+  return 1
+}
